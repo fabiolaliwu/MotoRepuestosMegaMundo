1facdb78e3440e48ad9543d6e08d3e00b08e76fd
Fabiola Li Wu <fabiliwu@hotmail.com>
2026-07-09 19:05:19 -0600

    updated routes

--- megamundo/src/App.jsx
+++ megamundo/src/App.jsx
@@ -1,7 +1,15 @@
 import Landing from './pages/landing/landing.jsx'
+import Catalogo from './pages/catalogo/catalogo.jsx'
 
 function App() {
-  return <Landing />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/catalogo" element={<Catalogo />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
