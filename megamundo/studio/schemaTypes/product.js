dbd470447aafeac91ee491c1876dad36ae38f239
Fabiola Li Wu <fabiliwu@hotmail.com>
2026-07-13 20:13:08 -0600

    chanhe the schema to input data

--- megamundo/studio/schemaTypes/product.js
+++ megamundo/studio/schemaTypes/product.js
@@ -4,10 +4,8 @@ export default {
   type: 'document',
   fields: [
     { name: 'name', title: 'Product Name', type: 'string' },
-    { name: 'price', title: 'Price', type: 'number' },
-    { name: 'category', type: 'reference', to: [{ type: 'category' }] },
     { name: 'brand', title: 'Brand', type: 'reference', to: [{ type: 'brand' }] },
-    { name: 'description', title: 'Description', type: 'string' }, 
+    { name: 'description', title: 'Description', type: 'string' },
     {
       name: 'variants',
       title: 'Product Variants',
@@ -15,33 +13,46 @@ export default {
       of: [{
         type: 'object',
         fields: [
-        { 
-          name: 'color', 
-          title: 'Color', 
-          type: 'string',
-          options: { list: ['Negro', 'Negro Matte','Blanco', 'Rojo', 'Azul', 'Gris', 'Mate'] } 
-        },
-        { 
-          name: 'sizes', 
-          title: 'Available Sizes', 
-          type: 'array',
-          of: [{ type: 'string' }], 
-          options: { 
-            list: ['S', 'M', 'L', 'XL', 'XXL'],
-            layout: 'checkbox' 
-          } 
-        },
-        { 
-          name: 'weight', 
-          title: 'Weight (kg)', 
-          type: 'number' 
-        },
-        { 
-          name: 'images', // Plural
-          title: 'Gallery Images', 
-          type: 'array', 
-          of: [{ type: 'image', options: { hotspot: true } }] 
-        }
+          {
+            name: 'samePriceForAll',
+            title: 'All sizes have the same price?',
+            type: 'boolean',
+            initialValue: true
+          },
+          {
+            name: 'price',
+            title: 'Price',
+            type: 'number',
+            hidden: ({ parent }) => !parent?.samePriceForAll
+          },
+          {
+            name: 'sizePricing',
+            title: 'Size & Pricing',
+            type: 'array',
+            hidden: ({ parent }) => parent?.samePriceForAll,
+            of: [{
+              type: 'object',
+              fields: [
+                { name: 'size', type: 'string', options: { list: ['S', 'M', 'L', 'XL', 'XXL'] } },
+                { name: 'price', type: 'number' }
+              ]
+            }]
+          },
+          { 
+            name: 'color', 
+            title: 'Color', 
+            type: 'string',
+            options: { list: ['Negro', 'Negro Matte', 'Blanco', 'Rojo', 'Azul', 'Gris', 'Mate'] } 
+          },
+          { 
+            name: 'sizes', 
+            title: 'Available Sizes', 
+            type: 'array',
+            of: [{ type: 'string' }], 
+            options: { list: ['S', 'M', 'L', 'XL', 'XXL'], layout: 'checkbox' } 
+          },
+          { name: 'mass', title: 'Weight (kg) / Volume (ml)', type: 'number' },
+          { name: 'images', title: 'Gallery Images', type: 'array', of: [{ type: 'image', options: { hotspot: true } }] }
         ]
       }]
     }
